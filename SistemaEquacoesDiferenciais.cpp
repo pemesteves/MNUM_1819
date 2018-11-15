d3eb1d674c5ac6c98d7d7967be0e678eca777510
Pedro Esteves <up201705160@fe.up.pt>
2018-11-15 14:41:21 +0000

    Changed rk2 and rk4 methods and add method to solve a diferencial equation of second order

--- SistemaEquacoesDiferenciais.cpp
+++ SistemaEquacoesDiferenciais.cpp
@@ -16,20 +16,22 @@ inline double dz(double x, double y, double z) { //dz/dx
 void euler(double x, double y, double z, double h) {
 	double yn = 0, zn = 0;
 
-	for (; x + h <= 1; x += h) {
+	for (int i = 0; i < 10; i++) {
 		yn = y + h * dy(x, y, z);
 		zn = z + h * dz(x, y, z);
 		y = yn; z = zn;
+		x += h;
 	}
 	cout << "(" << x << ", " << y << ", " << z << ")" << endl << endl;
 }
 
 void rk2(double x, double y, double z, double h) { //TODO Corrigir rk2
 	double yn = 0, zn = 0;
-	for (; x + h <= 1; x += h) {
-		yn = y + h * dy(x + h / 2, y + h * dy(x, y, z) / 2, z + dz(x, y, z) / 2);
-		zn = z + h * dz(x + h / 2, y + h * dy(x, y, z) / 2, z + dz(x, y, z) / 2);
+	for (int i = 0; i < 10; i++) {
+		yn = y + h * dy(x + h / 2, y + h * dy(x, y, z) / 2, z + h * dz(x, y, z) / 2);
+		zn = z + h * dz(x + h / 2, y + h * dy(x, y, z) / 2, z + h * dz(x, y, z) / 2);
 		y = yn; z = zn;
+		x += h;
 	}
 	cout << "(" << x << ", " << y << ", " << z << ")" << endl << endl;
 }
@@ -43,7 +45,7 @@ inline double dz1(double x, double y, double z, double h) {
 }
 
 inline double dy2(double x, double y, double z, double h) {
-	return h * dy(x + h / 2, y + dy1(x, y, z, h) / 2, z + dz1(x, y, z, h));
+	return h * dy(x + h / 2, y + dy1(x, y, z, h) / 2, z + dz1(x, y, z, h) / 2);
 }
 
 inline double dz2(double x, double y, double z, double h) {
@@ -68,10 +70,11 @@ inline double dz4(double x, double y, double z, double h) {
 
 void rk4(double x, double y, double z, double h) {
 	double yn = 0, zn = 0;
-	for (; x + h <= 1; x += h) {
+	for (int i = 0; i < 10; i++) {
 		yn = y + dy1(x, y, z, h) / 6 + dy2(x, y, z, h) / 3 + dy3(x, y, z, h) / 3 + dy4(x, y, z, h) / 6;
 		zn = z + dz1(x, y, z, h) / 6 + dz2(x, y, z, h) / 3 + dz3(x, y, z, h) / 3 + dz4(x, y, z, h) / 6;
 		y = yn;  z = zn;
+		x += h;
 	}
 	cout << "(" << x << ", " << y << ", " << z << ")" << endl << endl;
 }
